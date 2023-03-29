ff49ea4ce6f3f53f227f1cc7268a163c6b96ab90
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-03-29 16:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	const palindrome = (str) => {
+  const check = str.split('').reverse().join('')
+
+  return str === check;
+}
+
+
+// true
 
 }
 module.exports = palindrome

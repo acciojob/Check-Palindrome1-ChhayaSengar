bbc76b8c34662b0a91f959ecc9c3f7cb2d502ee7
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-03-30 16:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // complete the given function
 
 function palindrome(str){
-	var l = str.length;
-    for (var i = 0; i < l / 2; i++) {
-        if (str.charAt(i) !== str.charAt(l - 1 - i)) {
-            return false;
-        }
-    }
-    return true;
+	// var l = str.length;
+ //    for (var i = 0; i < l / 2; i++) {
+ //        if (str.charAt(i) !== str.charAt(l - 1 - i)) {
+ //            return false;
+ //        }
+ //    }
+ //    return true;
+
+
+	/* remove special characters, spaces and make lowercase*/
+  var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+  /* reverse removeChar for comparison*/
+  var checkPalindrome = removeChar.split('').reverse().join('');
+
+  /* Check to see if str is a Palindrome*/
+   if(removeChar === checkPalindrome){
+     return true;
+   }else{
+     return false;
+   }
 }
 
 
